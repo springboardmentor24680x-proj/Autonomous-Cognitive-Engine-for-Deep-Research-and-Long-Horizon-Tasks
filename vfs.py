c7c34012a8dd8cc1c56a02757280e80ec7eaa746
GAYATHRI-BHARGAVI <gayathribhargavi645564@gmail.com>
2026-01-05 19:04:03 +0530

    vfs.py

--- vfs.py
+++ vfs.py
@@ -4,20 +4,21 @@ class VirtualFileSystem:
 
     def write_file(self, filename, content):
         self.files[filename] = content
-        return f"✅ File '{filename}' written."
+        return f" File '{filename}' written."
 
     def read_file(self, filename):
         if filename not in self.files:
-            return f"❌ File '{filename}' not found."
+            return f" File '{filename}' not found."
         return self.files[filename]
 
     def edit_file(self, filename, content):
         if filename not in self.files:
-            return f"❌ File '{filename}' not found."
+            return f" File '{filename}' not found."
         self.files[filename] = content
         return f"✅ File '{filename}' updated."
 
     def ls(self):
         if not self.files:
-            return "📂 No files in memory."
+            return " No files in memory."
         return "\n".join(self.files.keys())
+
